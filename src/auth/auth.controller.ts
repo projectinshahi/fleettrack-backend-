d7aa2f2f7bd9c8288adac708ea355301da5aaa61
dk <kukkudanish@gmail.com>
2026-09-15 15:13:00 +0530

    final edit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -21,6 +21,7 @@ import { LoginDto } from './dto/login.dto';
 @Controller('auth')
 
 export class AuthController {
+  
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
